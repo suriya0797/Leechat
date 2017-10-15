a36c5974c6ba409e2f57e663341a85e7d3c909a1
manitamilnadu006 <manitamilnadu006@gmail.com>
2017-10-15 12:32:33 +0530

    Lee chat

---
+++ main/java/siva/leechat/Request.java
@@ -0,0 +1,17 @@
+package siva.leechat;
+
+
+public class Request {
+    private String status;
+    public Request() {
+        this.status=status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
